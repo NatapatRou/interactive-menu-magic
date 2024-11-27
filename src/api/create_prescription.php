1abfbce89898a17e6c59f370769df06ce116e034
NLut <Natapat2001@gmail.com>
2024-11-28 03:23:14 +0700

    Add try catch, update pharmacist status

--- src/api/create_prescription.php
+++ src/api/create_prescription.php
@@ -1,29 +1,46 @@
 <?php
 session_start();
 // create_presciption.php
+
+// CORS Headers
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: http://localhost:8080");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS"); // Allowed HTTP methods
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); // Allowed headers
-// Database connection
-//
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Include required files
 require_once __DIR__ . '/config.php';
 $API = require_once __DIR__ . "/api.php";
+
 $conn = new Connect();
 $data = json_decode(file_get_contents("php://input"), true);
-$avaiable_crew = $API->select_first_available();
-$pharmacist_id = $avaiable_crew[1]['id']; 
 
-$sql_sym = "INSERT INTO Prescription ( doctor_id, patient_id, pharmacist_id, medication_id, date_issued, status, notes )
-    VALUES (:doctor_id,:patient_id,:pharmacist_id,:medication_id,CURDATE(),'Pending',:notes)";
-$sym_stmt = $conn->prepare($sql_sym);
+try {
+    $available_crew = $API->select_first_available();
+    $pharmacist_id = $available_crew[1]['id'];
 
-$sym_stmt->bindParam(':doctor_id', $_SESSION['id']);
-$sym_stmt->bindParam(':patient_id', $data['patientId']);
-$sym_stmt->bindParam(':pharmacist_id', $pharmacist_id);
-$sym_stmt->bindParam(':medication_id', $data['medicineId']);
-$sym_stmt->bindParam(':notes', $data['prescription']);
+    $sql = "INSERT INTO Prescription 
+                (doctor_id, patient_id, pharmacist_id, medication_id, date_issued, status, notes)
+            VALUES 
+                (:doctor_id, :patient_id, :pharmacist_id, :medication_id, CURDATE(), 'Pending', :notes)";
 
-$sym_stmt->execute();
-echo json_encode(["status" => "success", "message" => "Create Prescription successfully"]);
+    $insert_stmt = $conn->prepare($sql);
+
+    $insert_stmt->bindParam(':doctor_id', $_SESSION['id']);
+    $insert_stmt->bindParam(':patient_id', $data['patientId']);
+    $insert_stmt->bindParam(':pharmacist_id', $pharmacist_id);
+    $insert_stmt->bindParam(':medication_id', $data['medicineId']); 
+    $insert_stmt->bindParam(':notes', $data['prescription']);
+    $insert_stmt->execute();
+    
+    $update_sql = "UPDATE Pharmacist SET status = 'Unavailable' WHERE id = :pharmacist_id ";
+    $update_stmt = $conn->prepare($update_sql);
+    $update_stmt->bindParam(':pharmacist_id', $pharmacist_id);
+    $update_stmt->execute();
+    
+    
+    echo json_encode(["status" => "success", "message" => "Prescription created successfully"]);
+} catch (Exception $e) {
+    echo json_encode(["status" => "error", "message" => "Invalid data"]);
+}
